81e9cd0366686325407c86ffbbc77091d5491338
Juan Abril <juanabca@unisabana.edu.co>
2024-08-02 18:08:53 -0500

    Se soluciona Responsabilidad unica y open/close principle

--- src/main/java/com/example/demo/unsolid/SalaryCalculator.java
+++ src/main/java/com/example/demo/unsolid/SalaryCalculator.java
@@ -3,7 +3,7 @@ package com.example.demo.unsolid;
 public class SalaryCalculator {
 
     public double calculateSalary(String name) {
-        EmployeeManager employeeManager = new EmployeeManager();
-        return employeeManager.calculateSalary(name);
+        // Calcular salario
+        return 0.0;
     }
 }
---
+++ src/main/java/com/example/demo/unsolid/SaveToFile.java
@@ -0,0 +1,7 @@
+package com.example.demo.unsolid;
+
+public class SaveToFile {
+    public void saveToFile(String name) {
+        // Guardar en archivo
+    }
+}
